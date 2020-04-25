1c8899dd37a852b1e0936a0d5542bdfecbf83716
RSibaja88 <54650378+RSibaja88@users.noreply.github.com>
2020-04-25 14:29:41 -0400

    set up router for app.js, added axios and router dom deps.

--- auth-friends/src/App.js
+++ auth-friends/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Router>
+        <div></div>
+      </Router>
+    </div>
   );
 }
 
